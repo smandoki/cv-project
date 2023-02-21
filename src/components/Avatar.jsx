64a3d3cd1e2c17d1790b28a9513d6ed166dfc23d
smandoki <80867148+smandoki@users.noreply.github.com>
2023-02-21 10:25:34 +0800

    Converted avatar to functional component

--- src/components/Avatar.jsx
+++ src/components/Avatar.jsx
@@ -1,43 +1,34 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import img from '../img/avatar-placeholder.png';
 import '../styles/avatar.css';
 
-class Avatar extends React.Component {
-  constructor() {
-    super();
+function Avatar() {
+  const fileInput = useRef();
+  const [file, setFile] = useState(null);
 
-    this.fileInput = React.createRef();
-    this.state = { file: null };
-  }
-
-  uploadPicture = (e) => {
-    e.preventDefault();
-    this.fileInput.current.click();
+  const uploadPicture = () => {
+    fileInput.current.click();
   };
 
-  changePicture = (e) => {
-    this.setState({ file: URL.createObjectURL(e.target.files[0]) });
+  const changePicture = (e) => {
+    setFile(URL.createObjectURL(e.target.files[0]));
   };
 
-  render() {
-    const { file } = this.state;
-
-    return (
-      <div className='avatar'>
-        <img src={file ? file : img} alt='avatar placeholder' />
-        <div className='change-avatar' onClick={this.uploadPicture}>
-          <i className='bi bi-camera'></i>
-          <p>Change Picture</p>
-        </div>
-        <input
-          type='file'
-          accept='image/*'
-          ref={this.fileInput}
-          onChange={this.changePicture}
-        />
+  return (
+    <div className='avatar'>
+      <img src={file ? file : img} alt='avatar placeholder' />
+      <div className='change-avatar' onClick={uploadPicture}>
+        <i className='bi bi-camera'></i>
+        <p>Change Picture</p>
       </div>
-    );
-  }
+      <input
+        type='file'
+        accept='image/*'
+        ref={fileInput}
+        onChange={changePicture}
+      />
+    </div>
+  );
 }
 
 export default Avatar;
